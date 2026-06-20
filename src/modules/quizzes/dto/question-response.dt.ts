ee8a17a54e33ab94ab2c5c8a11d2163d26e62843
pedja98 <predragmirkovic1998@gmail.com>
2026-06-20 23:08:59 +0200

    add swagger

--- src/modules/quizzes/dto/question-response.dt.ts
+++ src/modules/quizzes/dto/question-response.dt.ts
@@ -1,17 +1,22 @@
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger'
 import { IsNumber, IsString, IsArray, IsOptional, IsInt, Min } from 'class-validator'
 
 export class QuestionResponseDto {
   @IsNumber()
+  @ApiProperty({ type: Number })
   id!: number
 
   @IsString()
+  @ApiProperty({ type: String })
   text!: string
 
   @IsArray()
   @IsString({ each: true })
+  @ApiProperty({ type: Array<String> })
   options!: string[]
 
   @IsOptional()
+  @ApiPropertyOptional({ type: Number })
   @IsInt()
   @Min(0)
   correctOptionIndex?: number
